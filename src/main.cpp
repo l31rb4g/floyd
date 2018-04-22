eb72c6e3dbe9be8f5f5748a8f3931b94c1e37549
Gabriel Paladino <l31rb4g@gmail.com>
2018-04-22 14:02:20 -0300

    version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <QApplication>
 #include "window.h"
+#include <iostream>
 
+
+string VERSION = "0.0.1";
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    for (int i=0; i<argc; i++){
+        if (strcmp(argv[i], "--version")){
+            cout << "Floyd version " + VERSION << endl;
+            return 0;
+        }
+    }
 
     Window *window = new Window();
     window->show();
